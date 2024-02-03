bf6ba4836b8455e51c824d2bb8e6df777fe0fb3b
Mehak2716 <mehak.jindal_ftc@external.swiggy.in>
2024-02-04 01:35:53 +0530

    Included Position

---
+++ src/main/java/org/example/modals/Position.java
@@ -0,0 +1,31 @@
+package org.example.modals;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column){
+        if(row<0 || column<0)
+            throw new IllegalArgumentException("Position cannot be negative");
+
+        this.row = row;
+        this.column = column;
+    }
+
+    public boolean isValidNeighbourPosition(Position position){
+        boolean isNeighbor = Math.abs(row - position.row) <= 1 && Math.abs(column - position.column) <= 1;
+        return isNeighbor && !this.equals(position);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this)
+            return true;
+
+        if(!(obj instanceof Cell))
+            return false;
+
+        Position position = (Position) obj;
+        return this.row==position.row && this.column==position.column;
+    }
+}
---
+++ src/test/java/org/example/modals/PositionTest.java
@@ -0,0 +1,57 @@
+package org.example.modals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PositionTest {
+
+    @Test
+    public void TestPositionCreatedSuccessfully(){
+        assertDoesNotThrow(()->{
+            Position position = new Position(0,0);
+        });
+    }
+
+    @Test
+    public void TestPositionCreatedWithNegativeInput_ExpectException(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+           Position position = new Position(-1,-1);
+        });
+
+        String expectedMessage = "Position cannot be negative";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void TestIsValidNeighbourPositionForNeighbour_ExpectTrue(){
+        Position position = new Position(0,0);
+        Position neighbourPosition = new Position(0,1);
+
+        boolean isValid=position.isValidNeighbourPosition(neighbourPosition);
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void TestIsValidNeighbourPositionForNonNeighbour_ExpectFalse(){
+        Position position = new Position(0,0);
+        Position neighbourPosition = new Position(0,2);
+
+        boolean isValid=position.isValidNeighbourPosition(neighbourPosition);
+
+        assertFalse(isValid);
+    }
+
+    @Test
+    public void TestIsValidNeighbourPositionForDiaganolNeighbour_ExpectFalse(){
+        Position position = new Position(0,0);
+        Position neighbourPosition = new Position(1,1);
+
+        boolean isValid=position.isValidNeighbourPosition(neighbourPosition);
+
+        assertTrue(isValid);
+    }
+}
