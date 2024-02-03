1b61b09bb58da4087f8fad70b032aaf955b6ef9d
Mehak2716 <mehak.jindal_ftc@external.swiggy.in>
2024-02-03 22:33:50 +0530

    Refactored Cell and Cell Test

--- src/main/java/org/example/modals/Cell.java
+++ src/main/java/org/example/modals/Cell.java
@@ -1,6 +1,5 @@
 package org.example.modals;
 
-
 import org.example.constants.State;
 import org.example.constants.Symbol;
 
@@ -10,22 +9,21 @@ public class Cell {
 
     public Cell(State state) {
 
+        if(state==State.Stable)
+            throw new IllegalArgumentException("Cell can only be initilaized with Alive or Dead state");
+
         this.state = state;
         this.neighbours = new Neighbours();
     }
 
-    public int decision(){
-//        int aliveNeighbours= neighbours.countAlive();
-//        if(symbol==Symbol.X && (aliveNeighbours<2 || aliveNeighbours>3)){
-//            symbol = Symbol.O; // becomes dead
-//            return -1;
-//        }
-//        if(symbol==Symbol.O && aliveNeighbours==3){
-//            symbol = Symbol.X; //becomes alive
-//            return 1;
-//        }
-//        return 0;
-        return -1;
+    public State evolve() throws IllegalArgumentException{
+        int aliveNeighbours= neighbours.countAlive();
+        if ((isAlive() && (aliveNeighbours < 2 || aliveNeighbours > 3))
+                || (!isAlive() && aliveNeighbours == 3)) {
+            toggleState();
+            return this.state;
+        }
+        return State.Stable;
     }
 
     public void addNeighbour(Cell cell){
@@ -38,7 +36,6 @@ public class Cell {
     public void toggleState() {
         state = (state == State.Alive) ? State.Dead : State.Alive;
     }
-
     @Override
     public String toString() {
         return (state == State.Alive) ? Symbol.X.toString() : Symbol.O.toString();
--- src/test/java/org/example/modals/CellTest.java
+++ src/test/java/org/example/modals/CellTest.java
@@ -1,67 +1,178 @@
 package org.example.modals;
 
+import org.example.constants.State;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CellTest {
 
-//    @Test
-//    public void testDeadCellDecisionFor0AliveNeighbours_Expects0(){
-//        Cell cell = new Cell(Symbol.O);
-//        int result = cell.decision(0);
-//        assertEquals(0,result);
-//    }
-//    @Test
-//    public void testDeadCellDecisionFor1AliveNeighbours_Expects0(){
-//        Cell cell = new Cell(Symbol.O);
-//        int result = cell.decision(1);
-//        assertEquals(0,result);
-//    }
-//
-//    @Test
-//    public void testDeadCellDecisionFor3AliveNeighbours_Expects1(){
-//        Cell cell = new Cell(Symbol.O);
-//        int result = cell.decision(3);
-//        assertEquals(1,result);
-//    }
-//
-//    @Test
-//    public void testAliveCellDecisionFor0AliveNeighbours_ExpectsMinus1(){
-//        Cell cell = new Cell(Symbol.X);
-//        int result = cell.decision(0);
-//        assertEquals(-1,result);
-//    }
-//
-//    @Test
-//    public void testAliveCellDecisionFor1AliveNeighbours_ExpectsMinus1(){
-//        Cell cell = new Cell(Symbol.X);
-//        int result = cell.decision(1);
-//        assertEquals(-1,result);
-//    }
-//
-//    @Test
-//    public void testAliveCellDecisionFor3AliveNeighbours_Expects0(){
-//        Cell cell = new Cell(Symbol.X);
-//        int result = cell.decision(3);
-//        assertEquals(0,result);
-//    }
-//
-//    @Test
-//    public void testAliveCellDecisionFor4AliveNeighbours_ExpectsMinus1(){
-//        Cell cell = new Cell(Symbol.X);
-//        int result = cell.decision(4);
-//        assertEquals(-1,result);
-//    }
-//
-//    @Test
-//    public void testIfCellAliveForAliveCellExpectsTrue(){
-//        Cell cell = new Cell(Symbol.X);
-//       assertTrue(cell.isAlive());
-//    }
-//
-//    @Test
-//    public void testIfCellAliveForDeadCellExpectsFalse(){
-//        Cell cell = new Cell(Symbol.O);
-//        assertFalse(cell.isAlive());
-//    }
+    @Test
+    public void TestCellCreatedSuccessfully(){
+        assertDoesNotThrow(()->{
+            Cell cell = new Cell(State.Dead);
+        });
+    }
+
+    @Test
+    public void TestCreateCellWithStableState_ExpectException(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+           Cell cell = new Cell(State.Stable);
+        });
+
+        String expectedMessage = "Cell can only be initilaized with Alive or Dead state";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void TestAddedNeighbourSuccessfully(){
+        Cell cell = new Cell(State.Dead);
+        Cell neighbourCell = new Cell(State.Dead);
+
+        assertDoesNotThrow(()->{
+           cell.addNeighbour(neighbourCell);
+        });
+    }
+
+    @Test
+    public void TestIsCellAliveForAliveCell_ExpectTrue(){
+        Cell cell = new Cell(State.Alive);
+
+        boolean isAlive = cell.isAlive();
+
+        assertTrue(isAlive);
+    }
+
+    @Test
+    public void TestIsCellAliveForDeadCell_ExpectFalse(){
+        Cell cell = new Cell(State.Dead);
+
+        boolean isAlive = cell.isAlive();
+
+        assertFalse(isAlive);
+    }
+
+    @Test
+    public void TestToggleStateForDeadCell_ExpectAliveState(){
+        Cell cell = new Cell(State.Dead);
+
+        cell.toggleState();
+        boolean isAlive = cell.isAlive();
+
+        assertTrue(isAlive);
+    }
+
+    @Test
+    public void TestToggleStateForAliveCell_ExpectDeadState(){
+        Cell cell = new Cell(State.Alive);
+
+        cell.toggleState();
+        boolean isAlive = cell.isAlive();
+
+        assertFalse(isAlive);
+    }
+
+    @Test
+    public void TestCellEvolutionWhenNoNeighbour_ExpectException(){
+        Cell cell = new Cell(State.Dead);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            State evolve = cell.evolve();
+        });
+
+        String expectedMessage = "No Neighbour found";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void TestDeadCellEvolutionFor1AliveNeighbour_ExpectStableState(){
+        Cell cell = new Cell(State.Dead);
+        Cell neighbour = new Cell(State.Alive);
+
+        cell.addNeighbour(neighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Stable);
+    }
+
+    @Test
+    public void TestDeadCellEvolutionFor1DeadNeighbour_ExpectStableState(){
+        Cell cell = new Cell(State.Dead);
+        Cell neighbour = new Cell(State.Dead);
+
+        cell.addNeighbour(neighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Stable);
+    }
+
+    @Test
+    public void TestAliveCellEvolutionFor1DeadNeighbour_ExpectDeadState(){
+        Cell cell = new Cell(State.Alive);
+        Cell neighbour = new Cell(State.Dead);
+
+        cell.addNeighbour(neighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Dead);
+    }
+
+    @Test
+    public void TestAliveCellEvolutionFor1AliveNeighbour_ExpectDeadState(){
+        Cell cell = new Cell(State.Alive);
+        Cell neighbour = new Cell(State.Alive);
+
+        cell.addNeighbour(neighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Dead);
+    }
+
+    @Test
+    public void TestAliveCellEvolutionFor2AliveNeighbour_ExpectStableState(){
+        Cell cell = new Cell(State.Alive);
+        Cell firstNeighbour = new Cell(State.Alive);
+        Cell secondNeighbour = new Cell(State.Alive);
+
+        cell.addNeighbour(firstNeighbour);
+        cell.addNeighbour(secondNeighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Stable);
+    }
+
+    @Test
+    public void TestDeadCellEvolutionFor3AliveNeighbour_ExpectAliveState(){
+        Cell cell = new Cell(State.Dead);
+        Cell firstNeighbour = new Cell(State.Alive);
+        Cell secondNeighbour = new Cell(State.Alive);
+        Cell thirdNeighbour = new Cell(State.Alive);
+
+        cell.addNeighbour(firstNeighbour);
+        cell.addNeighbour(secondNeighbour);
+        cell.addNeighbour(thirdNeighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Alive);
+    }
+
+    @Test
+    public void TestAliveCellEvolutionFor4AliveNeighbour_ExpectDeadState(){
+        Cell cell = new Cell(State.Alive);
+        Cell firstNeighbour = new Cell(State.Alive);
+        Cell secondNeighbour = new Cell(State.Alive);
+        Cell thirdNeighbour = new Cell(State.Alive);
+        Cell fourthNeighbour = new Cell(State.Alive);
+
+        cell.addNeighbour(firstNeighbour);
+        cell.addNeighbour(secondNeighbour);
+        cell.addNeighbour(thirdNeighbour);
+        cell.addNeighbour(fourthNeighbour);
+        State state = cell.evolve();
+
+        assertEquals(state, State.Dead);
+    }
 }
